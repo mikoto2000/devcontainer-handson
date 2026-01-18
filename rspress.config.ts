3a5b2eb069eccba778e2dc5a4c7a1ee528133a4f
mikoto2000 <mikoto2000@gmail.com>
2026-01-18 02:20:25 +0000

    logo, なぜか 2 個定義しないと消えない？

--- rspress.config.ts
+++ rspress.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     light: '/rspress-light-logo.png',
     dark: '/rspress-dark-logo.png',
   },
+  logo: null,
   logoText: 'コンテナで構築する開発環境',
   themeConfig: {
     socialLinks: [],
